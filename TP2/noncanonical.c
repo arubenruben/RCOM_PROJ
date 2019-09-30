235685dfb1fa5dee2cee7eb34c599cd588012eed
LuisPRamos <49035388+LuisPRamos@users.noreply.github.com>
2019-09-30 13:05:53 +0100

    Add files via upload

--- TP2/noncanonical.c
+++ TP2/noncanonical.c
@@ -8,6 +8,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include "structs.h"
+
+#define ST_START 0
+#define ST_FLAG_RCV 1
+#define ST_A_RCV 2
+#define ST_C_RCV 3
+#define ST_BCC_OK 4
+#define ST_STOP 5
 
 
 #define BAUDRATE B38400
@@ -21,8 +29,10 @@ int main(int argc, char** argv)
 {
     int fd,c, res;
     struct termios oldtio,newtio;
-    char buf[255];
+    unsigned char buf[255];
     char unit_char_received[2]; //2 positions because of
+    int size=0;
+    int state = ST_START;
 
     if ( (argc < 2) || 
   	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
@@ -76,29 +86,109 @@ int main(int argc, char** argv)
     printf("New termios structure set\n");
 
 
-    int last_pos=0;
+    for (size = 0; state != ST_STOP; size++){
+      if (!read(fd,&buf[size],1)){
+        perror("Failled to read");
+        exit(1);
+      }
 
+      switch (state)
+      {
+      case ST_START:
+        //check FLAG byte
+        if(buf[size] == FLAG)
+          state = ST_FLAG_RCV;
+        break;
 
+      case ST_FLAG_RCV:
+        switch (buf[size]){
+          case A_EM:
+            //received A, go to state A received
+            state = ST_A_RCV;
+            break;
+          
+          case FLAG:
+            //received FLAG, same state
+            break;
+          
+          default:
+            //received other, go to start
+            state = ST_START;
+            break;
+        }
+        break;
+
+      case ST_A_RCV:
+        switch (buf[size]){
+          case C_SET:
+            //received C, go to state C received
+            state = ST_C_RCV;
+            break;
+          
+          case FLAG:
+            //received FLAG, go to flag state
+            state = ST_FLAG_RCV;
+            break;
+          
+          default:
+            //received other, go to start
+            state = ST_START;
+            break;
+        }
+        break;
+
+      case ST_C_RCV:
+        //received BCC, check BCC
+        if(buf[size] == (A_EM^C_SET))
+          //BCC correct
+          state = ST_BCC_OK;
+        else if(buf[size] == FLAG)
+          //Received FLAG
+          state = ST_FLAG_RCV;
+        else
+          //Received other
+          state = ST_START;
+        break;
+
+      case ST_BCC_OK:
+          //check FLAG byte
+          if(buf[size] == FLAG)
+            //received all, stop cycle
+            state = ST_STOP;
+          else 
+            //received other, go to start
+            state = ST_START;
+        break;
+
+      default:
+        state = ST_START;
+        break;
+      }
+    }
+    
+
+    /*    
+    size = 0;
     //Receving loop
-    while (STOP==FALSE) {       /* loop for input */
+    while (STOP==FALSE) {       //loop for input 
       
-      res = read(fd,&buf[last_pos],1);   //read one char
+      res = read(fd,&buf[size],1);   //read one char
       if(res==-1){
         printf("ERRORROROORORRO\n");
         return -1;
       }
 
     
-      if (buf[last_pos]=='*'){
+      if (buf[size]=='*'){
         STOP=TRUE;
-        buf[last_pos] = 0;
-        printf(":%s:%d\n", buf, last_pos+1);
+        buf[size] = 0;
+        printf(":%s:%d\n", buf, size+1);
       }
 
-      last_pos+=res;
+      size+=res;
   
     }
-
+    */
 
     sleep(1); //Resolve bug de receber lixo
     tcsetattr(fd,TCSANOW,&oldtio);
